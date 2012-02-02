4f72d3b3100cdc2d51412c55131f4e7cbbfbf0bf
Russell Moffitt <russmo@coralhead.com>
2012-02-02 18:37:42 +0000

    user login/out working, need to finish user registration

--- mpatlas/accounts/urls.py
+++ mpatlas/accounts/urls.py
@@ -8,46 +8,45 @@ urlpatterns = patterns('',
     (r'^logout/$', 
         'django.contrib.auth.views.logout', 
         {'template_name': 'accounts/logout.html'}),
-    '''
-    (r'^password_change/$', 
-        'django.contrib.auth.views.password_change', 
-        {'template_name': 'accounts/password_change_form.html'}),
     
-    (r'^password_change/done/$', 
-        'django.contrib.auth.views.password_change_done', 
-        {'template_name': 'accounts/password_change_done.html'}),
-    
-    (r'^password_reset/$', 
-        'django.contrib.auth.views.password_reset', 
-        {'template_name': 'accounts/password_reset_form.html',
-        'email_template_name': 'accounts/password_reset_email.html'}),
-    
-    (r'^password_reset/done/$', 
-        'django.contrib.auth.views.password_reset_done', 
-        {'template_name': 'accounts/password_reset_done.html'}),
-    
-    (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
-        'django.contrib.auth.views.password_reset_confirm', 
-        {'template_name': 'accounts/password_reset_confirm.html'}),
-    
-    (r'^reset/done/$', 
-        'django.contrib.auth.views.password_reset_complete', 
-        {'template_name': 'accounts/password_reset_complete.html'}),
-    
-    (r'^signup/$', 
-        'mysite.accounts.views.signup', 
-        {'template_name': 'accounts/signup_form.html',
-        'email_template_name': 'accounts/signup_email.html'}),
-    
-    (r'^signup/done/$', 
-        'mysite.accounts.views.signup_done', 
-        {'template_name': 'accounts/signup_done.html'}),
-    
-    (r'^signup/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
-        'mysite.accounts.views.signup_confirm'),
-    
-    (r'^signup/complete/$', 
-        'mysite.accounts.views.signup_complete', 
-        {'template_name': 'accounts/signup_complete.html'}),
-    '''
+    # (r'^password_change/$', 
+    #     'django.contrib.auth.views.password_change', 
+    #     {'template_name': 'accounts/password_change_form.html'}),
+    # 
+    # (r'^password_change/done/$', 
+    #     'django.contrib.auth.views.password_change_done', 
+    #     {'template_name': 'accounts/password_change_done.html'}),
+    # 
+    # (r'^password_reset/$', 
+    #     'django.contrib.auth.views.password_reset', 
+    #     {'template_name': 'accounts/password_reset_form.html',
+    #     'email_template_name': 'accounts/password_reset_email.html'}),
+    # 
+    # (r'^password_reset/done/$', 
+    #     'django.contrib.auth.views.password_reset_done', 
+    #     {'template_name': 'accounts/password_reset_done.html'}),
+    # 
+    # (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+    #     'django.contrib.auth.views.password_reset_confirm', 
+    #     {'template_name': 'accounts/password_reset_confirm.html'}),
+    # 
+    # (r'^reset/done/$', 
+    #     'django.contrib.auth.views.password_reset_complete', 
+    #     {'template_name': 'accounts/password_reset_complete.html'}),
+    # 
+    # (r'^signup/$', 
+    #     'mysite.accounts.views.signup', 
+    #     {'template_name': 'accounts/signup_form.html',
+    #     'email_template_name': 'accounts/signup_email.html'}),
+    # 
+    # (r'^signup/done/$', 
+    #     'mysite.accounts.views.signup_done', 
+    #     {'template_name': 'accounts/signup_done.html'}),
+    # 
+    # (r'^signup/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+    #     'mysite.accounts.views.signup_confirm'),
+    # 
+    # (r'^signup/complete/$', 
+    #     'mysite.accounts.views.signup_complete', 
+    #     {'template_name': 'accounts/signup_complete.html'}),
 )
