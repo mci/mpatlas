2bd431cdbbd87aa7ef72d235e7981903cc6ea264
Russell Moffitt <russmo@coralhead.com>
2012-02-14 23:33:15 +0000

    protection level display

--- mpatlas/mpa/admin.py
+++ mpatlas/mpa/admin.py
@@ -1,5 +1,9 @@
 from django.contrib.gis import admin
 from models import Mpa, MpaCandidate
 
-admin.site.register(Mpa, admin.GeoModelAdmin)
+class MpaAdmin(admin.GeoModelAdmin):
+    list_display = ('name', 'mpa_id', 'wdpa_id', 'country')
+    search_fields = ['name', 'country', 'mpa_id', 'wdpa_id']
+
+admin.site.register(Mpa, MpaAdmin)
 admin.site.register(MpaCandidate, admin.GeoModelAdmin)
