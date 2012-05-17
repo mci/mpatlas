63de080a1607e46341ead0d6bae257b7602d6dce
Russell Moffitt <russmo@coralhead.com>
2012-05-17 21:42:14 +0000

    minor fix to user registration
    Former-commit-id: 19eb7b06d2e025c0fff3628c53a4b17248ebc75e

--- mpatlas/accounts/models.py
+++ mpatlas/accounts/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_countries import CountryField
 from django.db.models.signals import post_save
 
 TITLE_CHOICES = (
-    ('', ''),
+    ('--', ''),
     ('Mr', 'Mr'),
     ('Mrs', 'Mrs'),
     ('Ms','Ms'),
@@ -17,8 +18,12 @@ class UserProfile(models.Model):
 
     # Other fields here
     title = models.CharField(max_length=200, default='', choices=TITLE_CHOICES)
-    affiliation = models.CharField('organization', max_length=300)
+    affiliation = models.CharField('organization', max_length=300, blank=True)
+    country_new = CountryField()
     country = models.CharField(max_length=300)
+    
+    def __unicode__(self):
+        return '%s - %s %s' % (self.user.username, self.user.first_name, self.user.last_name)
 
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
