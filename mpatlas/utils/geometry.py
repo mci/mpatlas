550d370f71d2eb78d878bc0ed3d0df0e6760984c
Russell Moffitt <russmo@coralhead.com>
2012-05-27 02:11:15 +0000

    new geometry massaging utils in utils.geometry, fixes invalid or wonky geoms and heals features split at the dateline to make a single unified geography feature.  Needs more testing.
    Former-commit-id: 472fbc5ef17c240e7739383950a208f6b95ea838

---
+++ mpatlas/utils/geometry.py
@@ -0,0 +1,59 @@
+from django.contrib.gis import geos, gdal
+from django.db import connection, transaction
+
+# unsafe use of "%s" % (varname) string format substitution, but this should never use values
+# from web users.  No chance of SQL injection here.
+
+@transaction.commit_on_success
+def fix_geom_dateline_raw(table, geomfield, pkfield, pk):
+    # First do a validity check, then fix topology using PostGIS 2.0 tools
+    cursor = connection.cursor()
+    cursor.execute("UPDATE %s SET %s = ST_MakeValid(%s) WHERE NOT (ST_IsValid(%s)) AND %s = %s" % (table, geomfield, geomfield, geomfield, pkfield, pk) )
+    transaction.set_dirty()
+    # If some coords are outside -180 to 180, shift things around until 
+    # everything is clipped between -180 to 180.
+    # Polygons across dateline will be split into 2 pieces on each side.
+    # This should now be safe to bring into PostGIS as a geometry or geography
+    cursor.execute("UPDATE %s SET %s = ST_WrapX(ST_WrapX(%s, 0, 360), 180, -360) WHERE ((ST_XMin(%s) < -180) OR (ST_XMax(%s) > 180)) AND %s = %s" % (table, geomfield, geomfield, geomfield, geomfield, pkfield, pk) )
+    transaction.set_dirty()
+    #transaction.commit_unless_managed()    
+
+def fix_geom_dateline(obj, geomfield='geom'):
+    table = obj._meta.db_table
+    pkfield = obj._meta.pk.name
+    pk = obj.pk
+    fix_geom_dateline_raw(table=table, geomfield=geomfield, pkfield=pkfield, pk=pk)
+
+@transaction.commit_on_success
+def geom2geog_raw(table, geomfield, geogfield, pkfield, pk):
+    cursor = connection.cursor()
+    # First set geog to geom as a default in all cases
+    cursor.execute("UPDATE %s SET %s = %s::geography WHERE %s = %s" % (table, geogfield, geomfield, pkfield, pk) )
+    # If polygon reaches across entire world, mostly likely because it's split
+    # at the dateline, with one piece touching -180 and another at 180.
+    # Try moving western hemisphere over 360 degrees (longitudes are now 0-360)
+    # Check new min/max, if they're less than 360, transform this shifted geometry 
+    # into geography, using a transform to plate caree and back to 4326 lat/lon
+    # as trick to normalize all coordinates to the range -180 to 180.
+    cursor.execute("""UPDATE %s SET 
+        %s = ST_Transform(ST_Transform(shift.geomshift, 32662), 4326)::geography
+        FROM (
+            SELECT ST_WrapX(t.%s, 0, 360) as geomshift
+            FROM %s as t
+            WHERE t.%s = %s AND ST_XMin(t.%s) = -180 AND ST_XMax(t.%s) = 180
+        ) as shift
+        WHERE %s = %s AND (ST_XMax(shift.geomshift) - ST_XMin(shift.geomshift) < 360)""" % (table, geogfield, geomfield, table, pkfield, pk, geomfield, geomfield, pkfield, pk) )
+    transaction.set_dirty()
+
+def geom2geog(obj, geomfield='geom', geogfield='geog'):
+    table = obj._meta.db_table
+    pkfield = obj._meta.pk.name
+    pk = obj.pk
+    geom2geog_raw(table=table, geomfield=geomfield, geogfield=geogfield, pkfield=pkfield, pk=pk)
+
+def geog2geom(geog):
+    # Transform to geometry
+    # small=UTM, polar=polar stereographic, large=mercator
+    # _ST_BestSRID(geog)
+    # _ST_BestSRID(geog1, geog2)
+    pass
