891a1455f1ec0c47913ee748bc00f0fe86733dd6
Russell Moffitt <russmo@coralhead.com>
2012-06-01 22:21:52 +0000

    ipad and IE support for maptip now complete.  Maptip is now a subclassed L.Popup.  Still need to fix map hover event and disable over maptip, clena up eveventing.
    Former-commit-id: 5cd8d8a9769728391cdecc4e9edd5d6c424b9c15

--- mpatlas/static/js/leaflet_maptip.js
+++ mpatlas/static/js/leaflet_maptip.js
@@ -1,6 +1,7 @@
 define([
   // These are path aliases configured in the requireJS bootstrap
-  'leaflet'
+  'leaflet',
+  'leaflet_utils'
 ], function(){
     L.Maptip = L.Popup.extend({
     	options: {
@@ -9,10 +10,11 @@ define([
     		maxHeight: null,
     		autoPan: true,
     		closeButton: true,
-    		offset: new L.Point(0, 2),
+    		offset: new L.Point(0, 0),
     		autoPanPadding: new L.Point(5, 5),
     		className: '',
     		closePopupOnClick: false
+    		//closePopupOnClick: true
     	},
 
     	onAdd: function (map) {
@@ -35,6 +37,93 @@ define([
     		this._update();
 
     		this._container.style.opacity = '1'; //TODO fix ugly opacity hack
+    	},
+     	
+     	_initLayout: function () {
+    		var prefix = 'maptip',
+    		    leafletprefix = 'leaflet-popup', // need this to make sure 3dtransform css from leaflet get applied properly in webkit, otherwise it will be unclickable
+    			container = this._container = L.DomUtil.create('div', leafletprefix + ' ' + prefix + ' ' + this.options.className),
+    			closeButton;
+
+    		if (this.options.closeButton) {
+     			closeButton = this._closeButton = L.DomUtil.create('a', 'leaflet-popup-close-button ' + prefix + '-close-button', container);
+     			closeButton.href = '#close';
+
+    			L.DomEvent.addListener(closeButton, 'click', this._onCloseButtonClick, this);
+    		}
+
+    		var wrapper = this._wrapper = L.DomUtil.create('div', prefix + '-content-wrapper', container);
+    		L.DomEvent.disableClickPropagation(wrapper);
+
+    		this._contentNode = L.DomUtil.create('div', prefix + '-content', wrapper);
+    		L.DomEvent.addListener(this._contentNode, 'mousewheel', L.DomEvent.stopPropagation);
+
+    		this._tipContainer = L.DomUtil.create('div', prefix + '-arrow-container', container);
+    		this._tip = L.DomUtil.create('div', prefix + '-arrow', this._tipContainer);
+    	},
+    	
+    	setLatLng: function (latlng) {
+    		this._latlng = latlng;
+    		this._update();
+    		return this;
+    	},
+    	
+    	setLayerPoint: function(point) {
+    	    if (!this._map) { return this; }
+    	    latlng_unbounded = this._map.layerPointToLatLngUnbounded(point);
+    	    return this.setLatLng(latlng_unbounded);
+    	},
+    	
+    	_updatePosition: function () {
+    		var pos = this._map.latLngToLayerPoint(this._latlng);
+
+            // this._containerBottom = -pos.y - this.options.offset.y;
+            // this._containerLeft = pos.x - Math.round(this._containerWidth / 2) + this.options.offset.x;
+            
+            // this._container.style.bottom = this._containerBottom + 'px';
+            // this._container.style.left = this._containerLeft + 'px';
+    		
+    		this._containerTop = pos.y + this.options.offset.y;
+    		this._containerLeft = pos.x + this.options.offset.x;
+
+    		this._container.style.top = this._containerTop + 'px';
+    		this._container.style.left = this._containerLeft + 'px';
+    	},
+    	
+    	_adjustPan: function () {
+    		if (!this.options.autoPan) { return; }
+
+    		var map = this._map,
+    			containerHeight = this._container.offsetHeight,
+    			containerWidth = this._containerWidth,
+    			statsContainer = L.DomUtil.get('stats_overlay'),
+    			statsWidth = (statsContainer.offsetWidth ? statsContainer.offsetWidth : 0)
+    			
+    			layerPos = new L.Point(
+    				this._containerLeft + 14 + statsWidth,
+    				this._containerTop - 29),
+    			
+    			containerPos = map.layerPointToContainerPoint(layerPos),
+    			adjustOffset = new L.Point(0, 0),
+    			padding      = this.options.autoPanPadding,
+    			size         = map.getSize();
+    		
+    		if (containerPos.x < 0) {
+    			adjustOffset.x = containerPos.x - padding.x;
+    		}
+    		if (containerPos.x + containerWidth > size.x) {
+    			adjustOffset.x = containerPos.x + containerWidth - size.x + padding.x;
+    		}
+    		if (containerPos.y < 0) {
+    			adjustOffset.y = containerPos.y - padding.y;
+    		}
+    		if (containerPos.y + containerHeight > size.y) {
+    			adjustOffset.y = containerPos.y + containerHeight - size.y + padding.y;
+    		}
+    		
+    		if (adjustOffset.x || adjustOffset.y) {
+    			map.panBy(adjustOffset);
+    		}
     	}
     });
     
