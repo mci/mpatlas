a61cb208e5aa591e80a753a3d88006dce529cd0f
Russell Moffitt <russmo@coralhead.com>
2012-09-27 21:32:44 +0000

    Allow rejected MPAs to still have a detail view, but not appear in lists and country lists

--- mpatlas/spatialdata/models.py
+++ mpatlas/spatialdata/models.py
@@ -13,8 +13,8 @@ class Nation(models.Model):
     
     @property
     def mpas(self):
-        from mpa.models import Mpa
-        return Mpa.objects.exclude(country=None).exclude(country='').exclude(verification_state='Rejected as MPA').filter(country=self.iso3code).order_by('name').only('mpa_id', 'name', 'designation', 'designation_eng')
+        from mpa.models import Mpa, MpaCandidate, mpas_all_nogeom, mpas_norejects_nogeom, mpas_noproposed_nogeom, mpas_proposed_nogeom
+        return mpas_norejects_nogeom.exclude(country=None).exclude(country='').filter(country=self.iso3code).order_by('name').only('mpa_id', 'name', 'designation', 'designation_eng')
     
     @models.permalink
     def get_absolute_url(self):
