e7176a2f68da3dec0ff6285b5f65fef87932dfe8
Russell Moffitt <russmo@coralhead.com>
2013-04-08 19:31:48 +0000

    adding verify state and designation fields to admin MPA display

--- mpatlas/mpa/admin.py
+++ mpatlas/mpa/admin.py
@@ -9,12 +9,30 @@ class CandidateInfoInline(admin.StackedInline):
     model = CandidateInfo
 
 class MpaAdmin(reversion.VersionAdmin, admin.GeoModelAdmin):
-    list_display = ('name', 'mpa_id', 'wdpa_id', 'country')
-    search_fields = ['name', 'country', 'mpa_id', 'wdpa_id']
+    list_display = ('name', 'english_designation', 'mpa_id', 'wdpa_id', 'country', 'sub_location', 'colored_verification_state')
+    search_fields = ['name', 'country', 'sub_location', 'mpa_id', 'wdpa_id']
     inlines = [
         WikiArticleInline,
         CandidateInfoInline,
     ]
+
+    def english_designation(self, obj):
+    	if (obj.designation == obj.designation_eng):
+    		return obj.designation
+    	else:
+    		return '%s (%s)' % (obj.designation, obj.designation_eng)
+    english_designation.admin_order_field = 'designation'
+    english_designation.short_description = 'Designation'
+
+    def colored_verification_state(self, obj):
+    	if (obj.verification_state == 'Rejected as MPA'):
+    		color_code = 'f00'
+    	else:
+    		color_code = '000'
+        return '<span style="color: #%s;">%s</span>' % (color_code, obj.verification_state)
+    colored_verification_state.allow_tags = True
+    colored_verification_state.admin_order_field = 'verification_state'
+    colored_verification_state.short_description = 'Verification state'
 
 admin.site.register(Mpa, MpaAdmin)
 admin.site.register(Contact, reversion.VersionAdmin)
