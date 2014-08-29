0e532568aa650fad03ac5e5528931be64e395367
Russell Moffitt <russmo@coralhead.com>
2014-08-29 11:34:33 -0700

    reverting gunicorn wsgi deploy script to gunicorn_django for now

--- mpatlas/deploy/run_gunicorn.sh
+++ mpatlas/deploy/run_gunicorn.sh
@@ -6,9 +6,6 @@ DJANGO_WSGI_MODULE=mpatlas.wsgi
 
 export PYTHONPATH=$PROJECTDIR:$PYTHONPATH
 
-USER=mpatlas
-GROUP=mpatlas
-
 # Change to project dir so gunicorn can find mpatlas.wsgi on default python search path
 cd $PROJECTDIR
 source ~/.bashrc
@@ -16,7 +13,6 @@ source $VIRTUALENV/bin/activate
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-# exec $VIRTUALENV/bin/gunicorn_django -c $PROJECTDIR/deploy/gunicorn.conf $PROJECTDIR/settings.py
-exec $VIRTUALENV/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
-  -c $PROJECTDIR/mpatlas/deploy/gunicorn.conf \
-  --user=$USER --group=$GROUP
+exec $VIRTUALENV/bin/gunicorn_django -c $PROJECTDIR/mpatlas/deploy/gunicorn.conf $PROJECTDIR/mpatlas/settings.py
+# exec $VIRTUALENV/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+#   -c $PROJECTDIR/mpatlas/deploy/gunicorn.conf
