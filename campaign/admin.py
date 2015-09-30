9323cdf6b7fb9a2606d373bdbac112bbdbafd8a2
Russell Moffitt <russmo@coralhead.com>
2015-09-30 15:52:18 -0700

    making Campaigns searchable in admin, list initiatives
    Former-commit-id: 0b66bf350bbe118eb84e4915e6a4a21e922a3641

--- campaign/admin.py
+++ campaign/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 from django.contrib.gis import admin as geoadmin
 from models import Campaign, Initiative
+from django.utils.html import strip_tags
 
 class CampaignAdmin(geoadmin.OSMGeoAdmin):
-	raw_id_fields = ('mpas',)
+    raw_id_fields = ('mpas',)
+    list_display = ('name', 'country', 'initiative_list', 'summary_excerpt')
+    search_fields = ['name', 'country', 'summary', 'initiative__name']
+
+    def summary_excerpt(self, obj):
+        if (obj.summary):
+            return strip_tags(obj.summary[:50]) + '...'
+        else:
+            return ''
+    summary_excerpt.admin_order_field = 'summary_excerpt'
+    summary_excerpt.short_description = 'Summary (excerpt)'
+
+    def initiative_list(self, obj):
+        return ', '.join(obj.initiative_set.values_list('name', flat=True))
+    initiative_list.admin_order_field = 'initiative_list'
+    initiative_list.short_description = 'Initiatives'
 
 class InitiativeAdmin(geoadmin.OSMGeoAdmin):
-	pass
+    pass
 
 admin.site.register(Campaign, CampaignAdmin)
 admin.site.register(Initiative, InitiativeAdmin)
