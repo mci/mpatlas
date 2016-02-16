3254d56a2ed85be0804fb23a9dd8bc3d62917336
Russell Moffitt <russmo@coralhead.com>
2016-02-16 10:51:33 -0800

    Switching to djrill Mandrill transactional email package, using sending_domain setting
    Former-commit-id: f71a7c75ef313892bd67b55f9f4fa13a2375ef37

--- mpatlas/settings.py
+++ mpatlas/settings.py
@@ -473,6 +473,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+
+    # Mandrill transactional email integration
+    'djrill',
 )
 
 # A sample logging configuration. The only tangible logging
