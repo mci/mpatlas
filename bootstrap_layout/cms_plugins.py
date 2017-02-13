6f1303eca3bc1407a1256fef13fc2e5bb990088b
Russell Moffitt <russmo@coralhead.com>
2017-02-13 13:55:57 -0800

    adding bootstrap_layout section plugin
    Former-commit-id: eeb4c76c67354ac9e74180caf16b61ddd36ec0f9

---
+++ bootstrap_layout/cms_plugins.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+from cms.plugin_pool import plugin_pool
+from cms.plugin_base import CMSPluginBase
+from django.conf import settings
+
+from . import models
+
+class SectionPlugin(CMSPluginBase):
+	model = models.Section
+	name = "Website Section"
+	module = 'Bootstrap Layout'
+	render_template = "bootstrap_layout/section.html"
+	allow_children = True
+	text_enabled = False
+	#raw_id_fields = ('group',)
+	admin_preview = False
+
+plugin_pool.register_plugin(SectionPlugin)
