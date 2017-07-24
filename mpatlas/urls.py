6308ad1fc9127b8a7bf48f67caa3926614e7993c
Russell Moffitt <russmo@coralhead.com>
2017-07-24 13:52:40 -0700

    adding debug_toolbar to debug stack
    Former-commit-id: 81524ae6b8e7cd895ec383001b6b62f606d3da80

--- mpatlas/urls.py
+++ mpatlas/urls.py
@@ -59,3 +59,8 @@ urlpatterns += i18n_patterns(
     prefix_default_language=False
 )
 
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
