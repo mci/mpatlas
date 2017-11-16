e2760337b4203e565b8bb7ebcb052c750a219205
Russell Moffitt <russmo@coralhead.com>
2017-11-16 10:58:17 -0800

    move Carto API key to local_settings.py
    Former-commit-id: 4123ed406501101f68be7763cfb751d66e2234c6

--- mpatlas/local_settings_EXAMPLE.py
+++ mpatlas/local_settings_EXAMPLE.py
@@ -1,47 +1,84 @@
-DEBUG = False
+# -*- coding: utf-8 -*-
 
-ADMINS = (
-    # ('Your Name', 'you@yourcompany.org'),
+from .default_settings import *
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+SITE_PATH = os.path.dirname(PROJECT_PATH)
+
+gettext = lambda s: s
+
+# Django settings for mpatlas project.
+
+DEBUG = True
+
+# CartoDB API Key, set this in local settings which is not publicly version controlled
+CARTO_API_KEY ='****'
+
+INSTALLED_APPS = INSTALLED_APPS + (
+    'debug_toolbar',
 )
 
-MANAGERS = ADMINS
+ADMINS = (
+    ('Admin', 'admin@domain.org'),
+)
+
+# MANAGERS = ADMINS
+
+# Email via Mandrill (MailChimp)
+DEFAULT_FROM_EMAIL = 'web_robot@mpatlas.org'
+SERVER_EMAIL = 'web_robot@mpatlas.org'
+EMAIL_HOST = 'smtp.mandrillapp.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'admin@domain.org'
+EMAIL_HOST_PASSWORD = '****'
 
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'mpatlas',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'USER': 'mpatlas',                      # Not used with sqlite3.
+        'PASSWORD': '****',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
+# Uncomment to enable caching with memcached
+CACHE_BACKEND = 'memcached://mpatlas-memcache.aaaa.0001.usw1.cache.amazonaws.com:11211'
+#CACHE_BACKEND = 'dummy://'
+CACHE_MIDDLEWARE_SECONDS = 2 * 60 / 1000
+CACHE_MIDDLEWARE_KEY_PREFIX = 'mpatlas_dev2'
+CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
+
 STORAGES_S3BOTO_MULTI = {
     'media' : {
-        'AWS_ACCESS_KEY_ID' : 'key',
-        'AWS_SECRET_ACCESS_KEY' : 'secret',
-        'AWS_STORAGE_BUCKET_NAME' : 'mybucket',
+        'AWS_ACCESS_KEY_ID' : '****',
+        'AWS_SECRET_ACCESS_KEY' : '****',
+        'AWS_STORAGE_BUCKET_NAME' : 'mpatlas',
         'AWS_LOCATION' : 'media',
     },
     'static' : {
-        'AWS_ACCESS_KEY_ID' : 'key',
-        'AWS_SECRET_ACCESS_KEY' : 'secret',
-        'AWS_STORAGE_BUCKET_NAME' : 'mybucket',
+        'AWS_ACCESS_KEY_ID' : '****',
+        'AWS_SECRET_ACCESS_KEY' : '****',
+        'AWS_STORAGE_BUCKET_NAME' : 'mpatlas',
         'AWS_LOCATION' : 'static',
     }
 }
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3botomulti.S3BotoStorage_media'
-STATICFILES_STORAGE = 'storages.backends.s3botomulti.S3BotoStorage_static'
+MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, os.pardir, 'media/'))
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, os.pardir, 'static/'))
+STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = os.path.join(STATIC_URL, 'admin/')
 
-# MEDIA and STATIC settings for S3/CloudFront
-MEDIA_ROOT = '/home/myuser/www/myproject/media/'
-MEDIA_URL = '//cdn.myproject.org/media/'
-STATIC_ROOT = '/home/myuser/www/myproject/static/'
-STATIC_URL = '//cdn.myproject.org/static/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+CKEDITOR_UPLOAD_PATH = 'media-uploads/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'secret'
+SECRET_KEY = '*****'
+
