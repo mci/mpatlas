101869f71f0e33bd387cd28a931e1cc6d68cc1e4
Russell Moffitt <russmo@coralhead.com>
2018-06-08 10:01:54 -0700

    Donate banner changes

--- mpatlas/static/js/donate_banner.js
+++ mpatlas/static/js/donate_banner.js
@@ -1,8 +1,14 @@
-define([
-  // These are path aliases configured in the requireJS bootstrap
-  'jquery',
-  'jquery.cookie'
-], function($){
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(['jquery', 'jquery.cookie'], factory);
+    } else if (typeof module === "object" && module.exports) {
+        var jquery = require('jquery');
+        var jqcookie = require('jquery.cookie');
+        module.exports = factory(jquery);
+    } else {
+        root.donate_banner = factory(root.jQuery);
+    }
+})(this, function($){
     var donatecookiename = 'mpatlas_saw_donate_30min';
     var setdonatecookie = function() {
         // set session cookie
@@ -17,13 +23,13 @@ define([
         return $.cookie(donatecookiename);
     };
 
-    var set_random_banner = function () {
+    var set_random_banner = function() {
         var bannerclasses = ["humpback-j4", "angelfish-j4", "turtle-j4", "manatee-j4", "otter-j4"];
         var randomclass = bannerclasses[Math.floor(Math.random() * bannerclasses.length)];
         $('#slidebanner').removeClass(bannerclasses.join(' ')).addClass(randomclass);
     };
 
-    var show_donate_banner = function (force) {
+    var show_donate_banner = function(force) {
         if (force || !hasdonatecookie()) {
             $('#slidebanner').hide().removeClass('small');
             $('#slidebanner').slideDown('slow', function() {
@@ -34,22 +40,26 @@ define([
         }
     };
 
-    set_random_banner();
-    // $('#slidebanner').hide();
-    // $("#slidebanner").css("position", "relative");
-    // $("#slidebanner").css("top", "0px");
-
-    $('.slidehide').click(function(e) {
-        e.preventDefault();
-        $('#slidebanner').slideUp('slow', function() {
-            // slide away complete
-            $('#slidebanner').addClass('small').show();
+    var ready_banner = function() {
+        set_random_banner();
+        $('.slidehide').click(function(e) {
+            e.preventDefault();
+            $('#slidebanner').slideUp('slow', function() {
+                // slide away complete
+                $('#slidebanner').addClass('small').show();
+            });
         });
-    });
+    };
+
+    try {
+        ready_banner();
+    } catch (e) {
+        //
+    }
 
     return {
         set_random_banner: set_random_banner,
-        show_donate_banner: show_donate_banner
+        show_donate_banner: show_donate_banner,
+        ready_banner: ready_banner
     };
-    // What we return here will be used by other modules
 });
