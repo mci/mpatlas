dbc1c3a445ae0e4df27841df2b25553199b67d53
Russell Moffitt <russ@russmoffitt.com>
2023-06-02 13:45:59 -0700

    update to simplify PrefixedSearchQuery
    Former-commit-id: 76806ea814f36acefc026149b2fdd5f0da8aabb4

--- postgres_search_extensions/search.py
+++ postgres_search_extensions/search.py
@@ -7,28 +7,11 @@ class PrefixedPhraseQuery(SearchQuery):
     Alter the tsquery executed by SearchQuery
     """
 
-    def __init__(self, value, all_partial=False, **kwargs):
+    def __init__(self, value, search_type="raw", all_partial=False, **kwargs):
         # all_partial=True puts :* after every term, otherwise only the last term is partial
         self.all_partial = all_partial
-        super(PrefixedPhraseQuery, self).__init__(value, **kwargs)
-
-    def as_sql(self, compiler, connection):
-        # Or <-> available in Postgres 9.6
-        glue = ':* & ' if self.all_partial else ' & '
-        value = adapt('%s:*' % glue.join(self.value.split()))
-
-        if self.config:
-            config_sql, config_params = compiler.compile(self.config)
-            template = 'to_tsquery({}::regconfig, {})'\
-                .format(config_sql, value)
-            params = config_params
-
-        else:
-            template = 'to_tsquery({})'\
-                .format(value)
-            params = []
-
-        if self.invert:
-            template = '!!({})'.format(template)
-
-        return template, params
+        glue = ":* & " if self.all_partial else " & "
+        value_prefix = "%s:*" % glue.join(value.split())
+        super(PrefixedPhraseQuery, self).__init__(
+            value_prefix, search_type=search_type**kwargs
+        )
