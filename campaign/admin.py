5c988ccf8e2e9bd8e6766c12d0edbb17c83b1911
Russell Moffitt <russ@russmoffitt.com>
2024-06-04 12:01:17 -0700

    use GISModelAdmin instead of OSM and GeoModelAdmin

--- campaign/admin.py
+++ campaign/admin.py
@@ -3,56 +3,65 @@ from django.contrib.gis import admin as geoadmin
 from .models import Campaign, Initiative, Organization
 from django.utils.html import strip_tags
 
-class CampaignAdmin(geoadmin.OSMGeoAdmin):
-    raw_id_fields = ('mpas',)
-    list_display = ('name', 'country', 'initiative_list', 'summary_excerpt')
-    search_fields = ['name', 'country', 'summary', 'initiative__name']
-    ordering = ('name',)
+
+class CampaignAdmin(geoadmin.GISModelAdmin):
+    raw_id_fields = ("mpas",)
+    list_display = ("name", "country", "initiative_list", "summary_excerpt")
+    search_fields = ["name", "country", "summary", "initiative__name"]
+    ordering = ("name",)
 
     def summary_excerpt(self, obj):
-        if (obj.summary):
-            return strip_tags(obj.summary[:50]) + '...'
+        if obj.summary:
+            return strip_tags(obj.summary[:50]) + "..."
         else:
-            return ''
-    summary_excerpt.admin_order_field = 'summary'
-    summary_excerpt.short_description = 'Summary (excerpt)'
+            return ""
+
+    summary_excerpt.admin_order_field = "summary"
+    summary_excerpt.short_description = "Summary (excerpt)"
 
     def initiative_list(self, obj):
-        return ', '.join(obj.initiative_set.values_list('name', flat=True))
-    # initiative_list.admin_order_field = 'initiative_list'
-    initiative_list.short_description = 'Initiatives'
+        return ", ".join(obj.initiative_set.values_list("name", flat=True))
 
-class InitiativeAdmin(geoadmin.OSMGeoAdmin):
-    list_display = ('name', 'campaign_excerpt', 'summary_excerpt')
-    search_fields = ['name', 'summary', 'campaigns__name']
-    ordering = ('name',)
+    # initiative_list.admin_order_field = 'initiative_list'
+    initiative_list.short_description = "Initiatives"
+
+
+class InitiativeAdmin(geoadmin.GISModelAdmin):
+    list_display = ("name", "campaign_excerpt", "summary_excerpt")
+    search_fields = ["name", "summary", "campaigns__name"]
+    ordering = ("name",)
 
     def campaign_list(self, obj):
-        return ', '.join(obj.campaigns.values_list('name', flat=True))
+        return ", ".join(obj.campaigns.values_list("name", flat=True))
+
     # campaign_list.admin_order_field = 'campaign_list'
-    campaign_list.short_description = 'Campaigns'
+    campaign_list.short_description = "Campaigns"
 
     def campaign_excerpt(self, obj):
         if self.campaign_list(obj):
-            return strip_tags(self.campaign_list(obj)[:50]) + '...'
+            return strip_tags(self.campaign_list(obj)[:50]) + "..."
         else:
-            return ''
+            return ""
+
     # campaign_excerpt.admin_order_field = 'campaigns'
-    campaign_excerpt.short_description = 'Campaign list (excerpt)'
+    campaign_excerpt.short_description = "Campaign list (excerpt)"
 
     def summary_excerpt(self, obj):
-        if (obj.summary):
-            return strip_tags(obj.summary[:50]) + '...'
+        if obj.summary:
+            return strip_tags(obj.summary[:50]) + "..."
         else:
-            return ''
+            return ""
+
     # summary_excerpt.admin_order_field = 'summary'
-    summary_excerpt.short_description = 'Summary (excerpt)'
+    summary_excerpt.short_description = "Summary (excerpt)"
+
 
 class OrganizationAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
-    list_display = ('name', 'nickname', 'website', 'social_handles')
-    search_fields = ['name', 'nickname', 'website', 'social_handles', 'summary']
-    ordering = ('name',)
+    list_display = ("name", "nickname", "website", "social_handles")
+    search_fields = ["name", "nickname", "website", "social_handles", "summary"]
+    ordering = ("name",)
+
 
 admin.site.register(Campaign, CampaignAdmin)
 admin.site.register(Initiative, InitiativeAdmin)
--- category/admin.py
+++ category/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
-#from django.contrib.gis import admin as geoadmin
 from .models import Category, Details, TaggedItem
+
 
 class DetailsInline(admin.StackedInline):
     model = Details
 
+
 class TaggedItemInline(admin.StackedInline):
     model = TaggedItem
 
+
 class CategoryAdmin(admin.ModelAdmin):
-    inlines = [
-        DetailsInline,
-        TaggedItemInline
-    ]
+    inlines = [DetailsInline, TaggedItemInline]
+
 
 admin.site.register(Category, CategoryAdmin)
